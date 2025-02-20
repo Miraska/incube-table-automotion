cd74797263b3a315381b0c2c163a4811d18b7ba7
DARKi <mehanik696137@gmail.com>
2025-02-20 22:21:22 +0300

    add:automiton manager

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,10 +1,24 @@
+import Types from "@configs";
+
+
 interface IJsonTriggerConfig {
     table: string;
     record: string;
 }
 
+// Доделать
+interface IAutomation {
+    id: string;
+    name: string;
+    trigger_config: IJsonTriggerConfig;
+    trigger_type: Types.Trigger;
+    enabled: boolean;
+    created_by: string;
+}
+
 namespace Interfaces {
     export type JsonTriggerConfig = IJsonTriggerConfig;
+    export type Automation = IAutomation;
 };
 
 export default Interfaces
