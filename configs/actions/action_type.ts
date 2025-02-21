0ed8a1dbc78afa48f4319e9f836fc91ad04e20d6
Komrad <mehanik696137@gmail.com>
2025-02-21 18:03:03 +0300

    add:actions and field configs

---
+++ configs/actions/action_type.ts
@@ -0,0 +1,3 @@
+type ActionType = "updateRecord" | "callAPI" | "sendNotification" | "runScript";
+
+export default ActionType;
---
+++ configs/fields/field_type.ts
@@ -0,0 +1,3 @@
+type FieldType = "number" | "string";
+
+export default FieldType;
