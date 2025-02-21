c5d5e3a76fb0d52ec4037a375eaf088fa24b7900
Komrad <mehanik696137@gmail.com>
2025-02-21 18:54:48 +0300

    upd: cmd parser and help command

--- lib/cmd/cmd_parser.ts
+++ lib/cmd/cmd_parser.ts
@@ -1,13 +1,19 @@
 import help from "./help"
 import LOG from "./log"
 
-function cmdParser(command: string): unknown {
+function cmdParser(command: string): void {
     switch (command) {
         case "exit":
             return LOG.success("Выход...")
         case "help":
             LOG.success("Вывод списока команд...\n")
             return help()
+        case "create":
+            return LOG.success("Вы пытались создать\n");
+        case "update":
+            return LOG.success("Вы пытались обнговить\n");
+        case "delete":
+            return LOG.success("Вы пытались удалить\n");
         default:
             return LOG.error("Неизвестная команда")
     }
